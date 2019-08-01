09e6140b7bfaf16a1ccbc32c3c0899f4500e58e0
shanti0x <danaciocoiu78@gmail.com>
2019-08-01 12:23:23 +0300

    Update Oms_orderDAOHibernateImpl.java

--- src/main/java/com/home/springboot/OMS/dao/Oms_orderDAOHibernateImpl.java
+++ src/main/java/com/home/springboot/OMS/dao/Oms_orderDAOHibernateImpl.java
@@ -57,9 +57,9 @@ public class Oms_orderDAOHibernateImpl implements Oms_orderDAO {
 	public Oms_order findByUserName(String user_name) {
 		Session currentSession = entityManager.unwrap(Session.class);
 
-		Oms_order theOms_userOrder = currentSession.createQuery("from Oms_order o where o.oms_user.user_name=" "" + user_name + "" " "" " "", Oms_order.class).GETresultlist ;
+		currentSession.createQuery("from Oms_order o where o.oms_user.user_name='" + user_name + "'", Oms_order.class).getResultList();
 
-		return theOms_userOrder;
+		return null;
 	}
 
 }
